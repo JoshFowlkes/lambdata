2304700bab9a864f8728d4100899fcddbc08ca1d
Joshua George Fowlkes <Inceptive@Joshua-George-Fowlkess-iMac.local>
2019-09-05 16:33:55 -0600

    adding thursday assignment updates

--- lambdaschool/Unit3/Week1/day2/test_assignment.py
+++ lambdaschool/Unit3/Week1/day2/test_assignment.py
@@ -1,5 +1,13 @@
 import unittest
 from assignment import Employee
 
+class TestEmployee(unittest.TestCase):
+    def test_email(self):
+        emp1 = Employee('Josh', 'Fowlkes', 100000)
+        emp2 = Employee('Escanor', 'Meliodas', 123456789)
 
+        self.assertEqual(emp1.email, 'Josh.Fowlkes@yahooooooooligans.com')
+        self.assertEqual(emp2.email, 'Escanor.Meliodas@yahooooooooligans.com')
 
+if __name__ == '__main__':
+    unittest.main()
